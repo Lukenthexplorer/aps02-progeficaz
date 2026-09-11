93fb3e8e173db8ff1cfa91397a355ad2d3b0cdce
lukenthexplorer <lucas.kenji2002@hotmail.com>
2026-09-11 10:49:52 -0300

    add: test for list all route

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,2 +1,6 @@
-from servidor import home
+from servidor import app
 
+def test_listar_imoveis():
+    cliente = app.test_client()
+    resposta = cliente.get("/imoveis")
+    assert resposta.status_code == 200
